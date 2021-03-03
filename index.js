e2decfca24b92e8ac706b10e9d10b6d7c869624f
tkvern <tkvern@qq.com>
2021-03-03 14:35:18 +0800

    优化匹配关键字

--- index.js
+++ index.js
@@ -3,15 +3,16 @@ const path = require('path')
 const resumeDir = './resume' // 文章所在目录
 
 const keywords = [
-  { key: 'github', weight: 10 },
-  { key: 'React', weight: 1 },
-  { key: 'react', weight: 1 },
-  { key: 'node.js', weight: 1 },
-  { key: 'mysql', weight: 1 },
+  { key: 'github.com', weight: 10 },
+  { key: 'React', weight: 3 },
+  { key: '小程序', weight: 3 },
+  { key: 'Node.js', weight: 1 },
   { key: '热爱技术', weight: 1 },
-  { key: '小程序', weight: 1 },
+  { key: '@gmail', weight: 2 },
+  { key: '本科', weight: 2 },
+  { key: '大专', weight: 1 },
 ]
-// 前端工程师筛选关键词
+// 前端工程师筛选关键词(React、小程序)
 
 const main = async () => {
   // 提取简历内容
